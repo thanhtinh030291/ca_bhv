29f1a86395b07d10a1dd5a5e8443009fe37b6303
thanhtinh <thanhtinh030291@gmail.com>
2021-03-30 15:54:47 +0700

    update

--- app/Http/Controllers/ClaimWordSheetController.php
+++ app/Http/Controllers/ClaimWordSheetController.php
@@ -109,6 +109,7 @@ class ClaimWordSheetController extends Controller
         };
         $MANTIS_BUG = MANTIS_BUG::where("project_id",1)->with(['CUSTOM_FIELD_STRING' => $condition_field_show, "BUG_TEXT"])->whereHas('CUSTOM_FIELD_STRING',$condition_field)->limit(7)->orderBy('id', 'DESC')->get();
         $claim_line = $member->ClaimLine;
+        
         //rmove claim line curent
         $arr_clli_oid = $HBS_CL_CLAIM->HBS_CL_LINE->pluck('clli_oid')->toArray();
         $claim_line = $claim_line->whereNotIn('clli_oid',$arr_clli_oid);
@@ -272,15 +273,7 @@ class ClaimWordSheetController extends Controller
                 $file_name_mantis =  md5(Str::random(11).time());
                 Storage::put('public/cache/' . $file_name_mantis, $file_contents);
                 $path_file[] = storage_path("app/public/cache/$file_name_mantis") ;
-                if($claimWordSheet->old_number_page_send != 0){
-                    
-                    $file_name_mantis_output =  md5(Str::random(9).time());
-                    $FirstPage = $claimWordSheet->old_number_page_send + 1 ;
-                    $cm_run ="gs -sDEVICE=pdfwrite -dNOPAUSE -dQUIET -dBATCH -dFirstPage={$FirstPage} -sOutputFile=". storage_path("app/public/cache/$file_name_mantis_output") ." ".storage_path("app/public/cache/$file_name_mantis");
-                    exec($cm_run);
-                    Storage::delete(str_replace(storage_path("app")."/", "", $path_file[1]));
-                    $path_file[1] = storage_path("app/public/cache/$file_name_mantis_output");
-                }
+                
             }
         }else{
             Storage::put('public/sortedClaim/' . $file_name .'.pdf', $pdf);
