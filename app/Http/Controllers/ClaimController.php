ffd5e1087fca8ae2c756e87db4c94865d2811e10
thanhtinhtoto <thanhtinh030291@gmail.com>
2021-06-14 16:44:43 +0700

    update

--- app/Http/Controllers/ClaimController.php
+++ app/Http/Controllers/ClaimController.php
@@ -1469,9 +1469,9 @@ class ClaimController extends Controller
         $content = str_replace('[[$ProvPstAmt]]', formatPrice(data_get($claim->hospital_request,'prov_gop_pres_amt')), $content);
         $content = str_replace('[[$ProDeniedAmt]]', formatPrice($sumAmountReject), $content);
         $content = str_replace('[[$ProvName]]', $Provider->prov_name, $content);
-        $content = str_replace('[[$bankNameProv]]', $Provider->cl_bank_name, $content);
-        $content = str_replace('[[$bankAddressProv]]', $Provider->cl_pay_bank_city, $content);
-        $content = str_replace('[[$acctNoProv]]', $Provider->cl_pay_acct_no, $content);
+        $content = str_replace('[[$bankNameProv]]', $Provider->bank_name, $content);
+        $content = str_replace('[[$bankAddressProv]]', $Provider->bank_branch, $content);
+        $content = str_replace('[[$acctNoProv]]', $Provider->acct_no, $content);
         $content = str_replace('[[$payeeProv]]', $Provider->payee, $content);
         $content = str_replace('[[$ProAddress]]', implode(",",$prov_address), $content);
         $content = str_replace('[[$Diagnosis]]', $Diagnosis, $content);
